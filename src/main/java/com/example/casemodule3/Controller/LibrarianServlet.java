927f64cde26ec61fb4ff274ab3ff16d9fd73edd6
nbt311 <nbtrung0311@gmail.com>
2023-12-05 23:41:54 +0700

    Thêm chuyển hướng đăng nhập cho admin & librarian

---
+++ src/main/java/com/example/casemodule3/Controller/LibrarianServlet.java
@@ -0,0 +1,23 @@
+package com.example.casemodule3.Controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LibrarianServlet", urlPatterns = "/librarian")
+public class LibrarianServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/librarian/home.jsp");
+        dispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
--- src/main/java/com/example/casemodule3/Model/Librarian.java
+++
@@ -1,9 +0,0 @@
-package com.example.casemodule3.Model;
-
-public class Librarian extends Admin {
-
-
-    public Librarian(String name, String dateOfBirth, String email, String password, String phone, String img) {
-        super(name, dateOfBirth, email, password, phone, img);
-    }
-}
